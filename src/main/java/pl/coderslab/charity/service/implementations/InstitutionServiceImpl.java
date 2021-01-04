4393c929146bb3140be50c0fc8f4995b2892bf68
root <mateusz.machnio.9@gmail.com>
2021-01-04 13:36:27 +0100

    created repository and service for Institution

--- src/main/java/pl/coderslab/charity/service/implementations/InstitutionServiceImpl.java
+++ src/main/java/pl/coderslab/charity/service/implementations/InstitutionServiceImpl.java
@@ -1,4 +1,44 @@
 package pl.coderslab.charity.service.implementations;
 
-public class InstitutionServiceImpl {
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repository.InstitutionRepository;
+import pl.coderslab.charity.service.interfaces.InstitutionService;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class InstitutionServiceImpl implements InstitutionService {
+
+    private final InstitutionRepository institutionRepository;
+
+    public InstitutionServiceImpl(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
+    @Override
+    public Institution findById(long id) {
+        return institutionRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("There is no such Institution"));
+    }
+
+    @Override
+    public Institution saveInstitution(Institution institution) {
+        return institutionRepository.save(institution);
+    }
+
+    @Override
+    public void updateInstitution(Institution institution) {
+        institutionRepository.save(institution);
+    }
+
+    @Override
+    public void deleteInstitution(long id) {
+        institutionRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Institution> findAllInstitutions() {
+        return institutionRepository.findAll();
+    }
 }
--- src/main/java/pl/coderslab/charity/service/interfaces/InstitutionService.java
+++ src/main/java/pl/coderslab/charity/service/interfaces/InstitutionService.java
@@ -1,4 +1,19 @@
 package pl.coderslab.charity.service.interfaces;
 
-public class InstitutionService {
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.entity.Institution;
+
+import java.util.List;
+
+public interface InstitutionService {
+
+    Institution findById(long id);
+
+    Institution saveInstitution(Institution institution);
+
+    void updateInstitution(Institution institution);
+
+    void deleteInstitution(long id);
+
+    List<Institution> findAllInstitutions();
 }

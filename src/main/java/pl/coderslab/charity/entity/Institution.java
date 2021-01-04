cdd7b37e4b4cde55d4d732884ac08478f9caa33d
root <mateusz.machnio.9@gmail.com>
2021-01-04 13:34:42 +0100

    created new Entity Institution

--- src/main/java/pl/coderslab/charity/entity/Institution.java
+++ src/main/java/pl/coderslab/charity/entity/Institution.java
@@ -1,4 +1,70 @@
 package pl.coderslab.charity.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = "institutions")
 public class Institution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Institution that = (Institution) o;
+        return id.equals(that.id) &&
+                name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Institution{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }

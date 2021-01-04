efe719cffe41bae68ad0f68b0154ea19c0fc1c23
root <mateusz.machnio.9@gmail.com>
2021-01-04 12:55:36 +0100

    created Entity Category

--- src/main/java/pl/coderslab/charity/entity/Category.java
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -1,4 +1,56 @@
 package pl.coderslab.charity.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id.equals(category.id) &&
+                name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }

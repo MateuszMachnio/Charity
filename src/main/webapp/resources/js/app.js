4f8158d63a8587c36a64f22be017318e472ba387
root <mateusz.machnio.9@gmail.com>
2021-01-10 22:58:56 +0100

    every time validation of the step 4, all fields are checked

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -174,9 +174,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     validationOfTheStep4(btn) {
       if (this.currentStep === 4) {
-        return this.checkField(btn, "ulicę", "#street", "#error1", /^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+(\s+[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)*\s+(\d+|(\d+(\/\d+)*))$/) &&
-            this.checkField(btn, "nazwę miasta", "#city", "#error2", /^([a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+|[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+(\s+[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)*)$/) &&
-            this.checkField(btn, "kod pocztowy", "#zipCode", "#error3", /^(\d{2}-\d{3})$/);
+        const checkStreet = this.checkField(btn, "ulicę", "#street", "#error1", /^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+(\s+[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)*\s+(\d+|(\d+(\/\d+)*))$/);
+        const checkCity = this.checkField(btn, "nazwę miasta", "#city", "#error2", /^([a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+|[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+(\s+[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)*)$/);
+        const checkZipCode = this.checkField(btn, "kod pocztowy", "#zipCode", "#error3", /^(\d{2}-\d{3})$/);
+        return checkStreet && checkCity && checkZipCode;
       }
       return true;
     }

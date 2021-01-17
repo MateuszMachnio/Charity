a5360211afb141e903178847e2a5812c3bc52e5b
root <mateusz.machnio.9@gmail.com>
2021-01-17 17:42:50 +0100

    created User entity

---
+++ src/main/java/pl/coderslab/charity/entity/User.java
@@ -0,0 +1,144 @@
+package pl.coderslab.charity.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 50)
+    @NotBlank
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Size(max = 50)
+    @NotBlank
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Size(min = 4)
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+
+    @Column(name = "created_on")
+    private LocalDateTime created;
+
+    @Column(name = "updated_on")
+    private LocalDateTime updated;
+
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    @PrePersist
+    public void setCreated() {
+        this.created = LocalDateTime.now();
+        this.role = "USER";
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    @PreUpdate
+    public void setUpdated() {
+        this.updated = LocalDateTime.now();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}

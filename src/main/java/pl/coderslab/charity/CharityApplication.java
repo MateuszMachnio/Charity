1a778f3bc242f15bd66dfe6d276a77309399a2e3
root <mateusz.machnio.9@gmail.com>
2021-01-18 14:31:00 +0100

    added file messages.properties

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -2,12 +2,22 @@ package pl.coderslab.charity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class CharityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
     }
 
 }

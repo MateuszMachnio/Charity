f7b0f69f20412758e4ceaac057042e82141f47ad
root <mateusz.machnio.9@gmail.com>
2021-01-18 13:19:58 +0100

    repository for User

--- src/main/java/pl/coderslab/charity/repository/UserRepository.java
+++ src/main/java/pl/coderslab/charity/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package pl.coderslab.charity.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
 }

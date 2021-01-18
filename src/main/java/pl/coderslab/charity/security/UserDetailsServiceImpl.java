baae04612021287a5139e9346faadde8fd2d3275
root <mateusz.machnio.9@gmail.com>
2021-01-18 13:21:24 +0100

    Spring Security configuration

--- src/main/java/pl/coderslab/charity/security/UserDetailsServiceImpl.java
+++ src/main/java/pl/coderslab/charity/security/UserDetailsServiceImpl.java
@@ -1,4 +1,42 @@
 package pl.coderslab.charity.security;
 
-public class UserDetailsServiceImpl {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if(user==null){
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                true, true, true, true, getGrantedAuthority(user));
+    }
+
+
+    private List<GrantedAuthority> getGrantedAuthority(User user){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+user.getRole()));
+        return authorities;
+    }
 }

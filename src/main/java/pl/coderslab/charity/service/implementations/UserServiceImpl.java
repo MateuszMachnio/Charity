c49000ac3c162f038045077d5a03efa8efeeb7a6
root <mateusz.machnio.9@gmail.com>
2021-01-18 14:40:21 +0100

    user service implementation

--- src/main/java/pl/coderslab/charity/service/implementations/UserServiceImpl.java
+++ src/main/java/pl/coderslab/charity/service/implementations/UserServiceImpl.java
@@ -1,4 +1,75 @@
 package pl.coderslab.charity.service.implementations;
 
-public class UserServiceImpl {
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repository.UserRepository;
+import pl.coderslab.charity.service.interfaces.UserService;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public User findById(long id) {
+        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("There is no such User"));
+    }
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
+
+    @Override
+    public User getCurrentUser() {
+        return userRepository.findByEmail(getPrincipal());
+    }
+
 }
--- src/main/java/pl/coderslab/charity/service/interfaces/UserService.java
+++ src/main/java/pl/coderslab/charity/service/interfaces/UserService.java
@@ -1,4 +1,26 @@
 package pl.coderslab.charity.service.interfaces;
 
+
+import pl.coderslab.charity.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User findById(long id);
+
+    User saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(long id);
+
+    List<User> findAllUsers();
+
+    User findByEmail(String email);
+
+    String getPrincipal();
+
+    User getCurrentUser();
+
 }
